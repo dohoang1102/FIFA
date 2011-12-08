51457ba834cd32aadb8c70c2e8ceef777da14a25
fomaa <roman.nofenko@gmail.com>
2011-12-08 17:58:44 +0200

    Added packages. Added EntityCF.

---
+++ Fifa.Repositories/MainContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Fifa.Repositories
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        public MainContext()
+            : base("name=Fifa")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+    }
+}
