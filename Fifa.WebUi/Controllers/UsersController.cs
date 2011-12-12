72f9ccb93a95b47c1f0af926fd5ae11ca1098dee
Constantin Titarenko <constantin.titarenko@gmail.com>
2011-12-12 02:09:16 +0200

    Generated views for users.

---
+++ Fifa.WebUi/Controllers/UsersController.cs
@@ -0,0 +1,140 @@
+using System.Linq;
+using System.Web.Mvc;
+using Fifa.WebUi.Models;
+
+namespace Fifa.WebUi.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository userRepository;
+
+        // If you are using Dependency Injection, you can delete the following constructor
+        public UsersController() : this(new UserRepository())
+        {
+        }
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        //
+        // GET: /Users/
+
+        public ViewResult Index()
+        {
+            return View(userRepository.All);
+        }
+
+        //
+        // GET: /Users/Details/5
+
+        public ViewResult Details(int id)
+        {
+            return View(userRepository.Find(id));
+        }
+
+        //
+        // GET: /Users/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Users/Create
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.IsAdmin = false;
+                userRepository.InsertOrUpdate(user);
+                userRepository.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Users/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            if (CanDoSelfEditing(id))
+            {
+                return View(userRepository.Find(id));
+            }
+            else
+            {
+                return new HttpUnauthorizedResult();
+            }
+        }
+
+        private bool CanDoSelfEditing(int id)
+        {
+            return User.Identity.IsAuthenticated &&
+                   userRepository.All.Any(x =>
+                                          x.Id == id &&
+                                          x.Name == User.Identity.Name);
+        }
+
+        //
+        // POST: /Users/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (CanDoSelfEditing(user.Id) && ModelState.IsValid)
+            {
+                user.IsAdmin = false;
+                userRepository.InsertOrUpdate(user);
+                userRepository.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Users/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            if (CanDoSelfEditing(id))
+            {
+                return View(userRepository.Find(id));
+            }
+            else
+            {
+                return new HttpUnauthorizedResult();
+            }
+        }
+
+        //
+        // POST: /Users/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (CanDoSelfEditing(id))
+            {
+                userRepository.Delete(id);
+                userRepository.Save();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return new HttpUnauthorizedResult();
+            }
+        }
+    }
+}
