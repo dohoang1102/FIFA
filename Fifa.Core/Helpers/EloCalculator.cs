0a0a35133220c2f6ab07fd83712f772c2b51d27b
arkirm <kirill.miroshnichenko@binary-studio.com>
2011-12-21 11:24:15 +0200

    Added calculator for ELO

---
+++ Fifa.Core/Helpers/EloCalculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Fifa.Core.Helpers
+{
+    /// <summary>
+    /// Calculates ratings of the players and their chances to win.
+    /// </summary>
+    public class EloCalculator
+    {
+        public const double MaxRatingGain = 100;
+        public const double SteadyRatingLimit = 1000;
+
+        private readonly double _maxGain;
+        private readonly double _steadyLimit;
+
+        private double _ratingA;
+        private double _ratingB;
+
+        public EloCalculator(double ratingPlayerA, double ratingPlayerB, double maxRatingGain = MaxRatingGain,
+            double steadyRatingLimit = SteadyRatingLimit)
+        {
+            _ratingA = ratingPlayerA;
+            _ratingB = ratingPlayerB;
+
+            _maxGain = maxRatingGain;
+            _steadyLimit = steadyRatingLimit;
+        }
+
+        public double ChanceToWinPlayerA
+        {
+            get { return calcChanceToWin(_ratingB, _ratingA); }
+        }
+
+        public double ChanceToWinPlayerB
+        {
+            get { return calcChanceToWin(_ratingA, _ratingB); }
+        }
+
+        public double RatingPlayerA
+        {
+            get { return _ratingA; }
+            set { _ratingA = value; }
+        }
+
+        public double RatingPlayerB
+        {
+            get { return _ratingB; }
+            set { _ratingB = value; }
+        }
+
+        public void WinGamePlayerA()
+        {
+            _ratingA += _maxGain * (1 - ChanceToWinPlayerA);
+
+            if (_ratingB >= _steadyLimit)
+                _ratingB -= _maxGain * ChanceToWinPlayerB;
+        }
+
+        public void WinGamePlayerB()
+        {
+            if (_ratingA >= _steadyLimit)
+                _ratingA -= _maxGain * ChanceToWinPlayerA;
+
+            _ratingB += _maxGain * (1 - ChanceToWinPlayerB);
+        }
+
+        public void DrawGame()
+        {
+            _ratingA += _maxGain * (1 - ChanceToWinPlayerA);
+            _ratingB += _maxGain * (1 - ChanceToWinPlayerB);
+        }
+
+        private double calcChanceToWin(double rating1, double rating2)
+        {
+            return 1 / (1 + Math.Pow(10, (rating1 - rating2) / 400));
+        }
+    }
+}
